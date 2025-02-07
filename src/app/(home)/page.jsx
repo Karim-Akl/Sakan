4ef4b808a24087b40c94a89f015f24dcd3d1a2f9
Karim Akl <karimakl@MacBook-Air-Karim.local>
2025-02-07 21:00:12 +0200

    Done logic Api Auth

---
+++ src/app/(home)/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-backgroundButton flex justify-center items-center w-full h-screen m-auto text-center">
+      <h1 className="text-white font-bold ">
+        Welcome back to the Sakan platform ... 😊
+      </h1>
+    </div>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export function Footer() {
+  return (
+    <footer className="mt-32">
+    footer
+    </footer>
+  )
+}
